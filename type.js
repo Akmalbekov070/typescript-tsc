6c3438bf847abd326b25a40cfb737b6ab73effd7
Akmalbekov070 <akmalbekov2022@gmail.com>
2023-12-27 22:41:24 +0300

    reference types

--- type.js
+++ type.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log('d');
+// malumot turlari
+// let a = 12
+// a = "text"
+// a  = false
+let a = 12;
+// let b = 'text';
+// b = 12
+let b;
+b = 'text';
+let c;
+// c = 12
+// c = 12
 //# sourceMappingURL=type.js.map
--- type.ts
+++ type.ts
@@ -1,1 +1,15 @@
-console.log('d');
+// malumot turlari
+
+// let a = 12
+// a = "text"
+// a  = false
+let a: number = 12;
+
+// let b = 'text';
+// b = 12
+let b: string;
+b = 'text';
+
+let c: boolean;
+// c = 12
+// c = 12
